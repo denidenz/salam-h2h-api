63264294d6b1666949297fbafa5b4fd49503eb22
denidenz <38670830+denidenz@users.noreply.github.com>
2026-04-18 02:22:02 +0700

    Remove signature verification from auth.js

--- h2h/auth.js
+++ h2h/auth.js
@@ -1,43 +1,9 @@
-const crypto = require("crypto");
-
 module.exports = async (req, res) => {
-  try {
-    const signature = req.headers["x-signature"];
-    const clientKey = req.headers["x-client-key"];
-    const timestamp = req.headers["x-timestamp"];
-
-    const data = `${clientKey}.${timestamp}`;
-
-    const verifier = crypto.createVerify("RSA-SHA256");
-    verifier.update(data);
-
-    const isValid = verifier.verify(
-      process.env.BSI_PUBLIC_KEY,
-      signature,
-      "base64"
-    );
-
-    if (!isValid) {
-      return res.json({
-        responseCode: "4037300",
-        responseMessage: "Auth Error"
-      });
-    }
-
-    const token = "TEST_TOKEN";
-
-    return res.json({
-      responseCode: "2007300",
-      responseMessage: "Auth Success",
-      accessToken: token,
-      tokenType: "BearerToken",
-      expiresIn: "900"
-    });
-
-  } catch (err) {
-    return res.json({
-      responseCode: "5007300",
-      responseMessage: "Auth Error"
-    });
-  }
+  return res.json({
+    responseCode: "2007300",
+    responseMessage: "Auth Success",
+    accessToken: "TEST_TOKEN",
+    tokenType: "BearerToken",
+    expiresIn: "900"
+  });
 };
