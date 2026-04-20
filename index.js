087e96ae66f389dac4e439d9f23892186f5c2bdb
denidenz <38670830+denidenz@users.noreply.github.com>
2026-04-20 17:21:09 +0700

    Refactor quotes and enhance root response message
    Updated string quotes to single quotes and modified response message.

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-const inquiry = require("./h2h/inquiry");
-const payment = require("./h2h/payment");
+const inquiry = require('./h2h/inquiry');
 
-app.post("/inquiry", inquiry);
-app.post("/payment", payment);
+app.post('/inquiry', inquiry);
 
-app.get("/", (req, res) => {
-  res.send("H2H RUNNING");
+app.get('/', (req, res) => {
+  res.send('H2H RUNNING 🚀');
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on port " + PORT));
+app.listen(PORT, () => console.log('Server running'));
