8b264fade3d3b35ff78e2e9032b16f80ac7a0f0b
denidenz <38670830+denidenz@users.noreply.github.com>
2026-04-20 18:16:35 +0700

    Add signature generation functionality
    Implement signature generation using RSA-SHA256.

---
+++ generate-signature.js
@@ -0,0 +1,21 @@
+const crypto = require("crypto");
+const fs = require("fs");
+
+// 🔑 PRIVATE KEY (punya kamu, BUKAN dari BSI)
+const privateKey = fs.readFileSync("./private_key.pem", "utf8");
+
+// 📌 DATA
+const clientKey = "SALAM212411";
+const timestamp = "2026-04-20T11:15:00+07:00";
+
+const data = `${clientKey}|${timestamp}`;
+
+// 🔐 SIGN
+const signer = crypto.createSign("RSA-SHA256");
+signer.update(data);
+signer.end();
+
+const signature = signer.sign(privateKey, "base64");
+
+console.log("SIGNATURE:");
+console.log(signature);
