519960e676be6a3c2b9479bcbc0293571e565f5a
denidenz <38670830+denidenz@users.noreply.github.com>
2026-04-20 18:58:30 +0700

    Refactor route handlers and remove duplicate requires

--- index.js
+++ index.js
@@ -3,17 +3,13 @@ const app = express();
 
 app.use(express.json());
 
+const inquiry = require('./h2h/inquiry');
+const payment = require('./h2h/payment');
 const createTransaction = require('./h2h/createTransaction');
 
 app.post('/create-transaction', createTransaction);
-
-const payment = require('./h2h/payment');
-
-app.post('/payment', payment);
-
-const inquiry = require('./h2h/inquiry');
-
 app.post('/inquiry', inquiry);
+app.post('/payment', payment);
 
 app.get('/', (req, res) => {
   res.send('H2H RUNNING 🚀');
