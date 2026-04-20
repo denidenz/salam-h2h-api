3e2270fd3834e701412776afadea52e09b68d2ec
denidenz <38670830+denidenz@users.noreply.github.com>
2026-04-21 03:24:13 +0700

    Refactor express.json middleware configuration
    Updated express.json middleware to use default encoding.

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ const express = require('express');
 const app = express();
 
 // ✅ HANYA INI (JANGAN DUPLIKAT)
-app.use(
-  express.json({
-    verify: (req, res, buf) => {
-      req.rawBody = buf.toString("utf8"); // 🔥 WAJIB
-    },
-  })
-);
-
+app.use(express.json({
+  verify: (req, res, buf) => {
+    req.rawBody = buf.toString();
+  }
+}));
 // 🔥 ROUTES
 const auth = require('./h2h/auth');
 const inquiry = require('./h2h/inquiry');
