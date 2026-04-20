e69ac04c0c9a35d784eac70a8cc88b0944915c53
denidenz <38670830+denidenz@users.noreply.github.com>
2026-04-20 18:56:30 +0700

    Add createTransaction endpoint to index.js

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+
+const createTransaction = require('./h2h/createTransaction');
+
+app.post('/create-transaction', createTransaction);
+
 const payment = require('./h2h/payment');
 
 app.post('/payment', payment);
