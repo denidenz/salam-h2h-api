d673cb1948064ee3e508e61116ef26900ebba3d6
denidenz <38670830+denidenz@users.noreply.github.com>
2026-04-20 13:33:55 +0700

    Add debug log middleware for request tracking
    Added a debug middleware to log incoming requests.

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const payment = require("./h2h/payment");
 const app = express();
 app.use(express.json());
 
+// DEBUG LOG
+app.use((req, res, next) => {
+  console.log("HIT:", req.method, req.url);
+  next();
+});
+
 app.post("/auth.php", auth);
 app.post("/inquiry.php", inquiry);
 app.post("/payment.php", payment);
