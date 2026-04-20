a2a2fcac87932417a15779de66c9dc4674ee3b1b
denidenz <38670830+denidenz@users.noreply.github.com>
2026-04-20 23:46:52 +0700

    Log request headers and body in payment.js
    Added logging for request headers and body in payment processing.

--- h2h/payment.js
+++ h2h/payment.js
@@ -49,6 +49,8 @@ module.exports = async (req, res) => {
 
     console.log("PAYMENT CUSTOMER:", cleanCustomerNo);
     console.log("FINAL RESPONSE: SUCCESS");
+    console.log("HEADERS:", req.headers);
+    console.log("BODY:", req.body);
 
     const docRef = db.collection("transactions").doc(cleanCustomerNo);
     const doc = await docRef.get();
