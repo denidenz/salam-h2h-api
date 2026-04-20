4f43c93495d2801ce017cd68a72ee14d34187499
denidenz <38670830+denidenz@users.noreply.github.com>
2026-04-21 00:35:27 +0700

    Refactor verifySignature to use rawBody

--- helper.js
+++ helper.js
@@ -1,23 +1,13 @@
 const crypto = require("crypto");
 
-function verifySignature({ clientKey, timestamp, signature, publicKey, body }) {
-  try {
-    // 🔥 stringify HARUS sama persis dengan BSI
-    const bodyString = JSON.stringify(body);
+function verifySignature({ clientKey, timestamp, signature, publicKey, rawBody }) {
+  const stringToSign = `${clientKey}|${timestamp}|${rawBody}`;
 
-    const data = `${clientKey}${timestamp}${bodyString}`;
+  console.log("STRING TO SIGN:", stringToSign);
 
-    console.log("SIGN STRING:", data);
+  const verifier = crypto.createVerify("RSA-SHA256");
+  verifier.update(stringToSign);
+  verifier.end();
 
-    const verifier = crypto.createVerify("RSA-SHA256");
-    verifier.update(data);
-    verifier.end();
-
-    return verifier.verify(publicKey, signature, "base64");
-  } catch (err) {
-    console.error("VERIFY ERROR:", err);
-    return false;
-  }
+  return verifier.verify(publicKey, signature, "base64");
 }
-
-module.exports = { verifySignature };
