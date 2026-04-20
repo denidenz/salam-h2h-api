d8bd3972e3cc9ac0018d92efc4a3474ef5a282d6
denidenz <38670830+denidenz@users.noreply.github.com>
2026-04-21 00:57:50 +0700

    Refactor verifySignature function for error handling

--- helper.js
+++ helper.js
@@ -1,26 +1,32 @@
 const crypto = require("crypto");
 
 function verifySignature(req) {
-  const signature = req.headers["x-signature"];
-  const timestamp = req.headers["x-timestamp"];
-  const endpoint = req.headers["endpoint-url"];
-  const authorization = req.headers["authorization"];
+  try {
+    const signature = req.headers["x-signature"];
+    const timestamp = req.headers["x-timestamp"];
+    const endpoint = req.headers["endpoint-url"];
+    const authorization = req.headers["authorization"];
 
-  const accessToken = authorization?.replace("Bearer ", "");
+    const accessToken = authorization?.replace("Bearer ", "");
 
-  const bodyString = JSON.stringify(req.body);
+    const bodyString = JSON.stringify(req.body);
 
-  const stringToSign = `${req.method}:${endpoint}:${bodyString}:${accessToken}:${timestamp}`;
+    const stringToSign = `${req.method}:${endpoint}:${bodyString}:${accessToken}:${timestamp}`;
 
-  console.log("STRING TO SIGN:", stringToSign);
+    console.log("STRING TO SIGN:", stringToSign);
 
-  const localSignature = crypto
-    .createHmac("sha256", process.env.CLIENT_SECRET)
-    .update(stringToSign)
-    .digest("base64");
+    const localSignature = crypto
+      .createHmac("sha256", process.env.CLIENT_SECRET)
+      .update(stringToSign)
+      .digest("base64");
 
-  console.log("LOCAL SIGN:", localSignature);
-  console.log("BSI SIGN :", signature);
+    console.log("LOCAL SIGN:", localSignature);
+    console.log("BSI SIGN :", signature);
 
-  return localSignature === signature;
+    return localSignature === signature;
+
+  } catch (err) {
+    console.error("VERIFY ERROR:", err);
+    return false;
+  }
 }
