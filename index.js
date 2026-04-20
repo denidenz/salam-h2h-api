0fa983f365afc81b65d87620107f91d9a1883064
denidenz <38670830+denidenz@users.noreply.github.com>
2026-04-20 16:54:02 +0700

    Refactor index.js for consistency and updates

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-// ✅ FIX PATH
-const auth = require('./h2h/auth')
-const inquiry = require('./h2h/inquiry')
-const payment = require('./h2h/payment')
+const inquiry = require("./h2h/inquiry");
+const payment = require("./h2h/payment");
 
-// ROUTES
-app.post('/auth.php', auth)
-app.post('/inquiry.php', inquiry)
-app.post('/payment.php', payment)
+app.post("/inquiry", inquiry);
+app.post("/payment", payment);
 
-app.listen(3000, () => {
-  console.log('H2H RUNNING 🚀')
-})
+app.get("/", (req, res) => {
+  res.send("H2H RUNNING");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
